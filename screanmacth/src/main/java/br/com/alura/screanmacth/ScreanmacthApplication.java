4d00d473cc242b36de4fcc398899378f8e022a33
Walace Nascimento <wlacens@hotmail.com>
2025-05-25 00:36:59 -0300

    implementado o Record DadosTemporada

--- screanmacth/src/main/java/br/com/alura/screanmacth/ScreanmacthApplication.java
+++ screanmacth/src/main/java/br/com/alura/screanmacth/ScreanmacthApplication.java
@@ -2,12 +2,16 @@ package br.com.alura.screanmacth;
 
 import br.com.alura.screanmacth.model.DadosEpisodio;
 import br.com.alura.screanmacth.model.DadosSerie;
+import br.com.alura.screanmacth.model.DadosTemporada;
 import br.com.alura.screanmacth.service.ConsumoAPI;
 import br.com.alura.screanmacth.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.yaml.snakeyaml.comments.CommentLine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreanmacthApplication implements CommandLineRunner {
@@ -30,11 +34,20 @@ public class ScreanmacthApplication implements CommandLineRunner {
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
 
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 
 
 		// API de imagem café
-//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
+		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		//System.out.println(json);
 
 	}
 }
