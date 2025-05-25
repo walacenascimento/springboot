4788833a54e89c8c089763f561ca399130b1a861
Walace Nascimento <wlacens@hotmail.com>
2025-05-24 21:24:38 -0300

    Alterando o readme

--- contador/src/main/java/br/com/alura/contador/contador/ContadorApplication.java
+++ contador/src/main/java/br/com/alura/contador/contador/ContadorApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.contador.contador;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContadorApplication {
+public class ContadorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContadorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Testando a aplicação");
+	}
 }
